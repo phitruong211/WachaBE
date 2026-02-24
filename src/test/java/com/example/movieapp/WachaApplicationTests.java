24dabf273d230865a0b583fba08de5369ccb9a67
Phitruong <phitruongpro211@gmail.com>
2026-02-24 20:31:46 +0700

    fix file test

--- src/test/java/com/example/movieapp/WachaApplicationTests.java
+++ src/test/java/com/example/movieapp/WachaApplicationTests.java
@@ -3,9 +3,11 @@ package com.example.movieapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
 @SpringBootTest
-class WachaApplicationTests {
 @ActiveProfiles("test")
+class WachaApplicationTests {
+
     @Test
     void contextLoads() {
     }
